fe0445d9b5f694c3d299e925cea3374c35f2fb99
Guilherme Vieira <guims02@hotmail.com>
2021-06-14 12:50:15 -0300

    códigos classe imc
    códigos em pacote dto e model

--- src/main/java/gui/vieira/provapoo/model/ImcController.java
+++ src/main/java/gui/vieira/provapoo/model/ImcController.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gui.vieira.provapoo.model;
 
-/**
- *
- * @author Note Moraes
- */
+import gui.vieira.provapoo.dto.ImcDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/imc")
 public class ImcController {
+    @GetMapping
+    public ResponseEntity<ImcDto> calcular(@RequestParam float peso){
+        return ResponseEntity.ok(new ImcDto(peso));
     
+    }
 }
