79b64d9f204b3b6409367ec731f2e88c1c0fb9e1
Guilherme Vieira <guims02@hotmail.com>
2021-06-14 12:41:34 -0300

    Codigos das classes
    novas dependências e código basico das classes

--- src/main/java/gui/vieira/provapoo/dto/ImcDto.java
+++ src/main/java/gui/vieira/provapoo/dto/ImcDto.java
@@ -10,5 +10,49 @@ package gui.vieira.provapoo.dto;
  * @author Note Moraes
  */
 public class ImcDto {
+    private float peso;
+    private float imc;
+    private String classificacao;
+    private String risco;
+
+    public ImcDto(float peso) {
+        this.peso = peso;
+    }
+    
+    
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getImc() {
+        return imc;
+    }
+
+    public void setImc(float imc) {
+        this.imc = imc;
+    }
+
+    public String getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(String classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    public String getRisco() {
+        return risco;
+    }
+
+    public void setRisco(String risco) {
+        this.risco = risco;
+    }
+    
+    
     
 }
